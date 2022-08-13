f0f2f398b1b3229f5010b921dfd073ab838d1796
cristianpolo <axelpolo9927@gmail.com>
2022-08-13 17:10:33 -0300

    realice ejercicio 7 de python

---
+++ Cristian.py
@@ -0,0 +1,7 @@
+#Ejercicio 7 Cristian 
+Historial3=(9530,4120,4580,1500,890,7516,426)
+promedio=sum(Historial3)/len(Historial3)
+print(promedio)
+if promedio > 4500:
+    print("Se ha excedido del gasto promedio para su mascota")
+
